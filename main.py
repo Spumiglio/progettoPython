4fc90e5154d3df2eff5eb8e1a7602e01dd05cd5c
luca <luca.gugole99@gmail.com>
2021-07-19 17:52:16 +0200

    implementation of read_data function and minor fix

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import random
+import folium
+import geopandas
+import shapely
 
 
-# Press the green button in the gutter to run the script.
+def read_data():
+    # import dello shapefile del Comune di Verona
+    city = geopandas.read_file('data/comune_verona.shp')
+    build = geopandas.read_file('data/edifici_verona.shp')
+    return city, build
+
+
+def show_map(gdf):
+    c = gdf.centroid
+    mmap = folium.Map(location=[c.geometry.y, c.geometry.x], tiles='OpenStreetMap', zoom_start=12)
+    folium.Marker([c.geometry.y, c.geometry.x], popup="<i>Popup di prova</i>", tooltip="Verona").add_to(mmap)
+    mmap.save('map.html')  # TODO only for test purpose
+    pass
+
+
+def generate_random_gps(boundaries):
+    return None
+
+
+def mark_area_around_bomb(location, mmap):
+    folium.Circle(location=location,
+                  radius=1000,
+                  popup="<b>Area ad alto rischio</b>",
+                  tooltip="ZONA ROSSA",
+                  color='red',
+                  fill=True,
+                  fill_color='#FF8989').add_to(mmap)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    cities, building = read_data()
+    show_map(cities)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
